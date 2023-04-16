11db5c922635086e34ee29a285ba9d3271b12988
Matt Plaumann <matt@plaumann.ca>
2023-04-15 21:04:14 -0400

    Scale as parameters, better zooming/centering behaviour

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 const App = () => (
-  <TimelineCollection name="Hinterlands of Fate's End" src='./assets/maps/fatesEnd.jpg'/>
+  <TimelineCollection name="Hinterlands of Fate's End" src='./assets/maps/fatesEnd.jpg' initialScale={0.5} minScale={0.1} maxScale={3}/>
 )
 
-function TimelineCollection({name, src}) {
+function TimelineCollection({name, src, initialScale, minScale, maxScale}) {
   return (
-    <TransformWrapper minScale={0.1} maxScale={3}>
+    <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
       <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
         <img alt={name} src={src}/>
       </TransformComponent>
