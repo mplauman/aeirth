2a64cab2501978475ec4bdc2bf1da30e989765aa
Matt Plaumann <matt@plaumann.ca>
2023-04-15 20:53:34 -0400

    Pinching, zooming, and panning

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 
 const App = () => (
-  <div>
-    <h1>Hello, world!</h1>
-  </div>
+  <TimelineCollection name="Hinterlands of Fate's End" src='./assets/maps/fatesEnd.jpg'/>
 )
+
+function TimelineCollection({name, src}) {
+  return (
+    <TransformWrapper minScale={0.1} maxScale={3}>
+      <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
+        <img alt={name} src={src}/>
+      </TransformComponent>
+    </TransformWrapper>
+  )
+}
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 
