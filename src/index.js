527f23655dccc77fb9583f50ad35a127b2f299d2
Matt Plaumann <matt@plaumann.ca>
2023-04-16 18:26:13 -0400

    Add loading screen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { KeepScale, ReactZoomPanPinchProps, TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
 import City from './assets/markers/city.svg';
 import FatesEndMap from './assets/maps/fatesEnd.jpg';
 import Style from './style.css';
 
-const App = () => (
-  <TimelineCollection name="Hinterlands of Fate's End" src={FatesEndMap} initialScale={0.5} minScale={0.1} maxScale={3}/>
-)
+const App = (props) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => { setLoaded(true); }, [loaded])
+
+  return (
+    loaded ? <TimelineCollection name="Hinterlands of Fate's End" src={FatesEndMap} initialScale={0.5} minScale={0.1} maxScale={3}/> : <Loading/>
+  )
+}
 
 const Marker = (props) => {
   return (
@@ -27,16 +33,22 @@ function MogCaern() {
   )
 }
 
-function TimelineCollection({name, src, initialScale, minScale, maxScale}) {
+const TimelineCollection = (props) => {
   return (
-    <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
+    <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={props.initialScale} minScale={props.minScale} maxScale={props.maxScale}>
       <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
         <div style={{position: "relative", background: "#999"}}>
-          <img alt={name} src={src}/>
+          <img alt={props.name} src={props.src}/>
           <MogCaern/>
         </div>
       </TransformComponent>
     </TransformWrapper>
+  )
+}
+
+const Loading = (props) => {
+  return (
+    <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>Loading...</div>
   )
 }
 
