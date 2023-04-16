0208ae60c96ecd367234ff991267e0c8d113032e
Matt Plaumann <matt@plaumann.ca>
2023-04-15 21:56:33 -0400

    Add a city marker, update webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+import { KeepScale, ReactZoomPanPinchProps, TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+import City from './assets/markers/city.svg';
 
 const App = () => (
   <TimelineCollection name="Hinterlands of Fate's End" src='./assets/maps/fatesEnd.jpg' initialScale={0.5} minScale={0.1} maxScale={3}/>
 )
 
+function Marker({x, y}) {
+  return (
+    <div style={{position: "absolute", top: y, left: x, zIndex: 2, transform: 'translate(-50%, -50%)' }}>
+      <KeepScale>
+        <City style={{ height: 53, width: 53 }}/>
+      </KeepScale>
+    </div>
+  )
+}
+
+function MogCaern() {
+  return (
+    <Marker x='2058px' y='1429px'/>
+  )
+}
+
 function TimelineCollection({name, src, initialScale, minScale, maxScale}) {
   return (
     <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
       <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
-        <img alt={name} src={src}/>
+        <div style={{position: "relative", background: "#999"}}>
+          <img alt={name} src={src}/>
+          <MogCaern/>
+        </div>
       </TransformComponent>
     </TransformWrapper>
   )
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,17 @@ module.exports = {
     path: path.join(__dirname, '/dist')
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      }
+    ]
   },
   plugins: [
     new HWP({template: path.join(__dirname, '/src/index.html')})
