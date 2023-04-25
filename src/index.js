2a49f946a8a61c8d31e1205adcc0ed450be870a0
Matt Plaumann <matt@plaumann.ca>
2023-04-25 08:06:44 -0400

    Enable strict mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
 import FatesEnd from './maps/FatesEnd';
 
 import './style.css';
@@ -10,4 +11,9 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App/>
+  </StrictMode>
+);
