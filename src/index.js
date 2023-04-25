1d94a37deaade2070508a5ba920ba19bc92a637d
Matt Plaumann <matt@plaumann.ca>
2023-04-25 07:50:15 -0400

    Refactor FatesEnd into its own component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Style from './style.css';
-import MapMarker from './components/MapMarker';
-import Map from './components/Map'
+import FatesEnd from './maps/FatesEnd';
 
 const App = () => {
   return (
-    <Map name="Hinterlands of Fate's End" asset={import('./assets/maps/fatesEnd.jpg')} initialScale={0.5} minScale={0.1} maxScale={3}>
-      <MapMarker x='2058px' y='1429px' type='city'/>
-    </Map>
+    <FatesEnd/>
   )
 }
 
