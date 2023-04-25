45651d6dc2b97ac7328913ba52b6dc06e992441a
Matt Plaumann <matt@plaumann.ca>
2023-04-25 07:32:33 -0400

    Refactor loading code into Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+import Loading from './Loading';
 
-const Map = ({initialScale, minScale, maxScale, children, name, image}) => {
+const Map = ({initialScale, minScale, maxScale, children, name, asset}) => {
+  const [image, setImage] = useState(null);
+
+  useEffect(
+    () => {
+      const delay = (time) => {
+        return new Promise(res => {
+          setTimeout(res, time);
+        });
+      }
+
+      const awaitMap = async () => {
+        await delay(500);
+        const m = await asset;
+
+        setImage(m.default);
+      }
+
+      awaitMap();
+    },
+    [image]
+  )
+
+  if (!image) {
+    return (
+      <Loading/>
+    )
+  }
+  
   return (
     <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
       <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import City from './assets/markers/city.svg';
 import Style from './style.css';
 import Marker from './components/Marker';
-import Loading from './components/Loading';
 import Map from './components/Map'
 
 const App = () => {
-  const [map, setMap] = useState(null);
-
-  useEffect(
-    () => {
-      const delay = (time) => {
-        return new Promise(res => {
-          setTimeout(res, time);
-        });
-      }
-
-      const loadMap = async () => {
-        await delay(500);
-        const m = await import('./assets/maps/fatesEnd.jpg');
-
-        setMap(m.default);
-      }
-
-      loadMap();
-    },
-    [map]
-  )
-
-  if (map) {
-    return (
-      <Map name="Hinterlands of Fate's End" image={map} initialScale={0.5} minScale={0.1} maxScale={3}>
-        <Marker x='2058px' y='1429px'>
-          <City/>
-        </Marker>
-      </Map>
-    )
-  }
-
   return (
-    <Loading/>
+    <Map name="Hinterlands of Fate's End" asset={import('./assets/maps/fatesEnd.jpg')} initialScale={0.5} minScale={0.1} maxScale={3}>
+      <Marker x='2058px' y='1429px'>
+        <City/>
+      </Marker>
+    </Map>
   )
 }
 
