0be0652b3099ada9b5fe86d2f4267bc4720c8a41
Matt Plaumann <matt@plaumann.ca>
2023-04-28 19:11:20 -0400

    Remove some unnecessary style

--- src/components/Map.js
+++ src/components/Map.js
@@ -34,7 +34,7 @@ const Map = ({initialScale, minScale, maxScale, children, name}) => {
   return (
     <TransformWrapper centerOnInit={true} centerZoomedOut={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
       <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
-        <div style={{position: "relative", background: "#999"}}>
+        <div>
           <img alt={name} src={image}/>
           {children.markers}
         </div>
