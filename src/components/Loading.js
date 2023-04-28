7204d9ee3d0bbe6f23716907cd28b25895452964
Matt Plaumann <matt@plaumann.ca>
2023-04-28 19:15:40 -0400

    Style fix for loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Loading = (props) => {
-    return (
-      <div style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}>Loading...</div>
-    )
-  }
+  return (
+    <div className='centered'>Loading...</div>
+  )
+}
   
 export default Loading;
