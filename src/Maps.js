1e82639214efe315ee275f1e2243cc2e6b7fb301
Matt Plaumann <matt@plaumann.ca>
2023-05-28 20:09:32 -0400

    Use callbacks for navigation, drop react-router (#11)

--- src/Maps.js
+++
@@ -1,8 +0,0 @@
-const Maps = {
-  FATES_END: {
-    title: 'The Hinterlands of Fate\'s End',
-    image: 'https://d1pyjk54yi0zpw.cloudfront.net/maps/fatesEnd.jpg',
-  },
-}
-
-export default Maps;
--- src/index.js
+++ src/index.js
@@ -1,81 +1,189 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './components/App';
-import Article from './components/Article';
-import ArticleLink from './components/ArticleLink';
-import Category from './components/Category';
-import Map from './components/Map';
-import MapMarker from './components/MapMarker';
 
 import './style.css';
 
-import Directory from './Directory';
+// Default configuration for the Fate's End map. It's used in a bunch of spots, use the spread
+// operator to populate stuff with these values then override specifics on a per use-case basis.
+const fatesEnd = {
+  title: 'The Hinterlands of Fate\'s End',
+  image: 'https://d1pyjk54yi0zpw.cloudfront.net/maps/fatesEnd.jpg',
+  initialScal: 0.5,
+  minScale: 0.1,
+  maxScale: 3,
+  markers: [],
+}
 
-const buildNavItems = (path, d) => {
-  return d.map( (item) => {
-    const itemPath = path + "/" + item.title;
-
-    if (item.children != null) {
-      return <Category key={itemPath} path={itemPath} title={item.title}>{ buildNavItems(itemPath, item.children) }</Category>;
-    }
-
-    return <ArticleLink key={itemPath} path={itemPath} title={item.title}/>
-  });
-};
-
-const buildRoutes = (path, d) => {
-  return d.flatMap( (item) => {
-    const itemPath = path + "/" + item.title;
-
-    if (item.children != null) {
-      return buildRoutes(itemPath, item.children);
-    }
-
-    return {
-      path: itemPath,
-      async lazy() {
-        const component = () => {
-          return (
-            <Article title={item.title} content={item.content}>
-              <Map
-                source={item.map}
-                initialScale={item.initialScale}
-                minScale={item.minScale}
-                maxScale={item.maxScale}
-              >
-                {
-                  item.markers.map((marker, index) => {
-                    return <MapMarker key={index} x={marker.x} y={marker.y} type={marker.type}/>
-                  })
-                }
-              </Map>
-            </Article>
-          );
-        };
-
-        return { Component: component }
-      }
-    };
-  })
-};
-
-const router = createBrowserRouter([
+const tocEntries = [
   {
-    path: '',
-    element: <App>{ buildNavItems('', Directory) }</App>,
+    title: 'Welcome',
+    article: {
+      module: import('./content/Welcome.md'),
+    }
+  },
+  {
+    title: 'Background',
     children: [
-      { 
-        index: true,
-        element: <></>
+      {
+        title: 'Gods',
+        map: null,
+        article: {
+          title: 'The Gods of Aeirth',
+          module: import('./content/Background/Gods of Aeirth.md'),
+        }
       },
-    ].concat(buildRoutes('', Directory))
-  }
-])
+      {
+        title: 'Calendar',
+        map: null,
+        article: {
+          title: 'The Calendar of Aeirth',
+          module: import('./content/Background/The Calendar.md'),
+        }
+      },
+    ]
+  },
+  {
+    title: 'Campaign - The Watchmans',
+    children: [
+      {
+        title: 'Overview',
+        article: {
+          title: 'Campaign Overview',
+          module: import('./content/Watchmans/Overview.md'),
+        }
+      },
+      {
+        title: 'Timeline',
+        children: [
+          {
+            title: '231.12.21',
+            map: fatesEnd,
+            article: {
+              title: 'A Murder Scene Most Fowl',
+              module: import('./content/Watchmans/231.12.21 - A Murder Scene Most Fowl.md'),
+            },
+          },
+          {
+            title: '231.13.06',
+            map: fatesEnd,
+            article: {
+              title: 'The Assignment',
+              module: import('./content/Watchmans/231.13.06 - The Assignment.md'),
+            },
+          },
+          {
+            title: '231.13.07',
+            map: fatesEnd,
+            article: {
+              title: 'A Watch Failure',
+              module: import('./content/Watchmans/231.13.07 - A Watch Failure.md'),
+            },
+          },
+          {
+            title: '231.14.01',
+            map: fatesEnd,
+            article: {
+              title: 'Safe Haven',
+              module: import('./content/Watchmans/231.14.01 - Safe Haven.md'),
+            },
+          },
+          {
+            title: '231.14.02',
+            map: fatesEnd,
+            article: {
+              title: 'Instigating and Investigating',
+              module: import('./content/Watchmans/231.14.02 - Instigating and Investigating.md'),
+            },
+          },
+          {
+            title: '231.14.03',
+            map: fatesEnd,
+            article: {
+              title: 'Mutual Ambushing',
+              module: import('./content/Watchmans/231.14.03 - Mutual Ambushing.md'),
+            },
+          },
+          {
+            title: '231.14.04',
+            map: fatesEnd,
+            article: {
+              title: 'The Report',
+              module: import('./content/Watchmans/231.14.04 - The Report.md'),
+            },
+          },
+        ]
+      },
+    ]
+  },
+  {
+    title: 'Characters and Factions',
+    children: [
+      {
+        title: 'Oggogul the Monk',
+        article: {
+          title: 'Oggogul',
+          module: import('./content/Characters and Factions/Oggogul.md'),
+        },
+      },
+      {
+        title: 'Pally',
+        article: {
+          title: 'Pally the Paladin',
+          module: import('./content/Characters and Factions/Pally.md'),
+        },
+      },
+      {
+        title: 'Romin',
+        article: {
+          title: 'Sergeant Romin',
+          module: import('./content/Characters and Factions/Romin.md'),
+        },
+      },
+    ]
+  },
+  {
+    title: 'Locations',
+    children: [
+      {
+        title: 'Mog Caern',
+        map: fatesEnd,
+        article: {
+          title: 'Mog Caern',
+          module: import('./content/Locations/Mog Caern.md'),
+        },
+      },
+      {
+        title: 'Roadside Camp',
+        map: fatesEnd,
+        article: {
+          title: 'Roadside Camp',
+          module: import('./content/Locations/Roadside Camp.md'),
+        },
+      },
+      {
+        title: 'Sly Fox Inn',
+        map: fatesEnd,
+        article: {
+          title: 'Sly Fox Inn',
+          module: import('./content/Locations/Sly Fox Inn.md'),
+        },
+      },
+      {
+        title: 'The Salt Wastes',
+        map: fatesEnd,
+        article: {
+          title: 'The Salt Wastes',
+          module: import('./content/Locations/The Salt Wastes.md'),
+        },
+      },
+    ]
+  },
+];
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App tocEntries={tocEntries}/>
   </React.StrictMode>
 );
