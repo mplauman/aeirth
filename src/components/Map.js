d4464777230487ff814b8f52c8e55ab1c4001a2c
Matt Plaumann <matt@plaumann.ca>
2023-06-03 16:10:21 -0400

    Display centered image

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch';
+import React, { useState } from 'react';
 
 const Map = ({initialScale, minScale, maxScale, title, image, children}) => {
+  const [state, setState] = useState({
+    dx: 0,
+    dy: 0,
+  })
+
+  const onImageLoad = async ({target}) => {
+    setState({
+      ...state,
+      dx: state.dx - target.naturalWidth / 2,
+      dy: state.dy - target.naturalHeight / 2,
+    });
+  }
+
   return (
-    <TransformWrapper limitToBounds={false} centerOnInit={true} initialScale={initialScale} minScale={minScale} maxScale={maxScale}>
-      <TransformComponent wrapperStyle={{width: "100%", height: "100%"}}>
-        <div>
-          <img alt={title} src={image}/>
-          {children}
-        </div>
-      </TransformComponent>
-    </TransformWrapper>
+    <img onLoad={onImageLoad} style={{position: "fixed", left: 0, top: 0, transform: "translate(50vw, 50vh) translate(" + state.dx + "px, " + state.dy + "px)"}} src={image} alt={title}/>
   )
 }
 
