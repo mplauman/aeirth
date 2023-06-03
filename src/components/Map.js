491fe4019e443076104fc22dba387b2e260407ea
Matt Plaumann <matt@plaumann.ca>
2023-06-03 19:50:20 -0400

    handle mouse and touch events

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,83 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Map = ({initialScale, minScale, maxScale, title, image, children}) => {
   const [state, setState] = useState({
     dx: 0,
     dy: 0,
   })
+
+  useEffect(() => {
+    window.addEventListener('wheel', handleMouseWheel, { passive: false });
+
+    window.addEventListener('mousedown', handleMouseDown, { passive: false });
+
+    window.addEventListener('touchstart', handleTouchStart, { passive: false });
+
+    return () => {
+      window.removeEventListener('wheel', handleMouseWheel, { passive: false });
+
+      window.removeEventListener('mousedown', handleMouseDown, { passive: false });
+      window.removeEventListener('mousemove', handleMouseMove, { passive: false });
+      window.removeEventListener('mouseup', handleMouseUp, { passive: false });
+
+      window.removeEventListener('touchstart', handleTouchStart, { passive: false });
+      window.removeEventListener('touchmove', handleTouchMove, { passive: false });
+      window.removeEventListener('touchend', handleTouchEnd, { passive: false });
+    }
+  })
+
+  const handleMouseWheel = (args) => {
+    console.log("Mouse wheel", args);
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+
+  const handleMouseDown = (args) => {
+    console.log("Mouse down", args);
+    window.addEventListener('mousemove', handleMouseMove, { passive: false });
+    window.addEventListener('mouseup', handleMouseUp, { passive: false });
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+  const handleMouseUp = (args) => {
+    console.log("Mouse up", args);
+    window.removeEventListener('mousemove', handleMouseMove, { passive: false });
+    window.removeEventListener('mouseup', handleMouseUp, { passive: false });
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+  const handleMouseMove = (args) => {
+    console.log("Mouse moved", args);
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+
+  const handleTouchStart = (args) => {
+    console.log('touch start', args);
+    window.addEventListener('touchmove', handleTouchMove, { passive: false });
+    window.addEventListener('touchend', handleTouchEnd, { passive: false });
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+  const handleTouchMove = (args) => {
+    console.log('touch move', args);
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
+  const handleTouchEnd = (args) => {
+    console.log('touch end', args);
+    window.removeEventListener('touchmove', handleTouchMove, { passive: false });
+    window.removeEventListener('touchend', handleTouchEnd, { passive: false });
+    args.stopPropagation();
+    args.preventDefault();
+    return false;
+  }
 
   const onImageLoad = async ({target}) => {
     setState({
@@ -15,7 +88,7 @@ const Map = ({initialScale, minScale, maxScale, title, image, children}) => {
   }
 
   return (
-    <img onLoad={onImageLoad} style={{position: "fixed", left: 0, top: 0, transform: "translate(50vw, 50vh) translate(" + state.dx + "px, " + state.dy + "px)"}} src={image} alt={title}/>
+    <img onLoad={onImageLoad} draggable={false} style={{position: "fixed", left: 0, top: 0, transform: "translate(50vw, 50vh) translate(" + state.dx + "px, " + state.dy + "px)"}} src={image} alt={title}/>
   )
 }
 
