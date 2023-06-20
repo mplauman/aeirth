7253463de59fc98cfce42d02747e5e2d96f8a3b3
Matt Plaumann <matt@plaumann.ca>
2023-06-19 23:24:05 -0400

    Add wikilink plugin

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from 'react';
 import InformationDrawer from './InformationDrawer';
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import wikiLinkPlugin from 'remark-wiki-link';
 
 const Article = ({title, content}) => {
   return (
     <InformationDrawer title={title}>
-      <ReactMarkdown children={content} remarkPlugins={[remarkGfm]}/>
+      <ReactMarkdown
+        children={content}
+        remarkPlugins={[
+          remarkGfm,
+          wikiLinkPlugin,
+        ]}
+      />
     </InformationDrawer>
   )
 }
