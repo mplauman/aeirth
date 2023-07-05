e2a33caa31c00f8ed7993a996fe138b6d23da32a
Matt Plaumann <matt@plaumann.ca>
2023-07-04 21:06:40 -0400

    Inline the table of contents

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
 
 import Article from './Article';
+import ArticleLink from './ArticleLink';
+import Category from './Category';
 import Map from './Map';
-import TableOfContents from './TableOfContents';
+import CloseIcon from './icons/close.svg';
+import MenuIcon from './icons/menu.svg';
 
 import {
   createHashRouter,
@@ -52,7 +58,20 @@ const buildRoutes = (context, tocEntries) => {
   return routes
 };
 
+
+const buildNavigation = (tocEntries) => {
+  return tocEntries.map( (tocEntry, index) => {
+    if (tocEntry.children != null) {
+      return <Category key={index} tocEntry={tocEntry}>{ buildNavigation(tocEntry.children) }</Category>;
+    }
+
+    return <ArticleLink key={index} tocEntry={tocEntry}/>
+  });
+};
+
 const App = ({context, tocEntries}) => {
+  const [tocOpen, setTocOpen] = useState(false)
+
   return (
     <RouterProvider
       router={
@@ -63,7 +82,16 @@ const App = ({context, tocEntries}) => {
               <div className='mainView'>
                 <Outlet/>
               </div>
-              <TableOfContents>{tocEntries}</TableOfContents>
+              <Drawer anchor='left' open={tocOpen} onClose={() => setTocOpen(false)}>
+                <Box className='tableOfContents' sx={{ width: 'auto', overflowY: 'scroll' }} role='presentation' onClick={() => setTocOpen(false)} onKeyDown={() => setTocOpen(false)}>
+                  <ul>
+                    { buildNavigation(tocEntries) }
+                  </ul>
+                </Box>
+              </Drawer>
+              <div onClick={() => setTocOpen(!tocOpen)} className='tableOfContentsButton'>
+                {tocOpen ? <CloseIcon/> : <MenuIcon/>}
+              </div>
             </>,
             children: buildRoutes(context, tocEntries),
           }
