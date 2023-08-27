1af488a46178f10c275ccdedad86ee0ea9f5d3e2
Matt Plaumann <matt@plaumann.ca>
2023-08-27 13:54:55 -0400

    Workarounds for style stuff I'm probably breaking (#25)

--- src/components/TextArticle.js
+++ src/components/TextArticle.js
@@ -9,10 +9,10 @@ const TextArticle = () => {
 
   return (
     <div className='informationDrawer phb'>
-      <div className='toolbar' style={{ position: 'fixed', top: '10px', left: '70px', display: 'flex', height: '60px', width: 'calc(100vw - 70px)' }}>
-        <div className='title'><h1 style={{ lineHeight: '100%' }} className='title'>{data.title}</h1></div><div/>
+      <div className='toolbar' style={{ position: 'fixed', top: '10px', left: '70px', display: 'flex', height: '60px', width: 'calc(100% - 70px)' }}>
+        <div className='title'><h1 style={{ lineHeight: '100%' }} className='title'>{data.title}</h1></div>
       </div>
-      <div style={{ position: 'fixed', top: '70px' }} className='content'>
+      <div style={{ position: 'fixed', top: '70px', width: 'calc(100vw - 2em)', height: 'calc(100% - 70px)'}} className='content'>
         <Markdown campaign={data.campaign} content={data.content}/>
       </div>
     </div>
